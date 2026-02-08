c3129f1b9fea11098d19bc91ded6c0291c553020
ZeroBuglab <python1bgit5217@gmail.com>
2026-02-08 22:20:32 +0500

    Add files via upload

---
+++ handgame.py
@@ -0,0 +1,26 @@
+import random
+
+count = 0
+game_count = 0
+names = {1:"Камень", 2:"Бумага", 3:"Ножницы"}
+
+while True:
+    gg = random.randint(1, 3)
+    you = input("выбери:1 (Камень) или 2 (Бумага) или 3 (Ножницы)")
+    if you ==  "конец":
+        break
+    you =int(you)
+    print("Компьюетер:", names[gg])
+    if you == str(gg) :
+        print("Ничья")
+    elif (you == 1 and gg == 3) or (you == 2 and gg == 1) or (you == 3 and gg == 2):
+        print("WIN")
+        count += 1
+    else:
+        print("LOSE")
+        game_count += 1
+
+
+print("счетчик", count,"/",game_count )
+
+
